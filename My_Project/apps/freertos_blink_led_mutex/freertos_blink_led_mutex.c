575b07427715ef07dc3473cefa4626defa4aae65
Hyunok Kim <hokim72@gmail.com>
2015-11-03 11:26:57 +0900

    Add FreeRTOS examples

---
+++ My_Project/apps/freertos_blink_led_mutex/freertos_blink_led_mutex.c
@@ -0,0 +1,155 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+#include "xil_printf.h"
+#include "xil_cache.h"
+#include "xparameters.h"
+#include "xstatus.h"
+#include "xgpio.h"
+
+#define GPIO_DEVICE_ID	XPAR_LEDS_4BITS_DEVICE_ID
+
+#define LED_CHANNEL	1
+
+#define BLINK_PERIOD 10
+
+#define mainLED_ON_TASK_PRIORITY	(tskIDLE_PRIORITY + 1)
+#define mainLED_OFF_TASK_PRIORITY	(tskIDLE_PRIORITY + 1)
+
+static void prvLed_ON(void* pvParameters);
+static void prvLed_OFF(void* pvParameters);
+int prvSetGpioHardware(void);
+void prvLed_Toggle(int Mode);
+
+XGpio GpioOutput;
+
+xSemaphoreHandle xMutex_Led = NULL;
+
+// This example illustrates synchronization of two tasks using mutex.
+// The example shows the usage of Mutex and different non-priority based
+// scheduling algorithms.
+// Two theads of same priority are created to toggle the 4-bit LED. One thread
+// puts the LED in ON state and the other in OFF state. The toggle LED function
+// is protected by a Mutex.
+// Each thread calls toggle LED function with appropriate argument (ON or OFF).
+// Toggle LED function takes the mutex and toggles the LED. To ensure 50%
+// duty cycle, the thread that takes the mutex sleeps keeping the LED in the
+// relevant state. Because of the scheduling algorithm, the control transfers
+// to other thread which agin calls the toggle LED function with appropriate 
+// argument.
+// This thread tries to take the Mutex and blocks as it is held by first thread.
+// After the first thread comes out of sleep, it release the mutex and then
+// yields so that the second thread can toggle the LED to then relevant state.
+// Second thread then puts the LED in appropriate state and sleeps. This
+// control flow goes on.
+
+int main(void)
+{
+    microblaze_disable_interrupts();
+
+    #if defined( XPAR_MICROBLAZE_USE_ICACHE ) && ( XPAR_MICROBLAZE_USE_ICACHE != 0 )
+    {
+        Xil_ICacheInvalidate();
+        Xil_ICacheEnable();
+    }
+    #endif
+
+    #if defined( XPAR_MICROBLAZE_USE_DCACHE ) && ( XPAR_MICROBLAZE_USE_DCACHE != 0 )
+    {
+        Xil_DCacheInvalidate();
+        Xil_DCacheEnable();
+    }
+    #endif
+
+	xMutex_Led = xSemaphoreCreateMutex();
+
+	configASSERT(xMutex_Led);
+
+	// Setup the GPIO Hardware.
+	prvSetGpioHardware();
+
+	// Start the two tasks.
+	xTaskCreate(prvLed_ON, "LED_ON",
+				configMINIMAL_STACK_SIZE, NULL,
+				mainLED_ON_TASK_PRIORITY, NULL);
+
+	xTaskCreate(prvLed_OFF, "LED_OFF",
+				configMINIMAL_STACK_SIZE, NULL,
+				mainLED_OFF_TASK_PRIORITY, NULL);
+
+	// Start the tasks and timer running.
+	vTaskStartScheduler();
+
+	// If all is well, the scheduler will now be running, and the following line
+	// will never be reached. If the following line does execute, then there was
+	// insufficient FreeRTOS heap memory available for the idle and/or timer
+	// tasks to be created. See the memory management section on the FreeRTOS
+	// web site for more details.
+	for (;;);
+}
+
+static void prvLed_ON(void* pvParameters)
+{
+	for (;;)
+	{
+		prvLed_Toggle(0x1);
+		taskYIELD();
+	}
+}
+
+static void prvLed_OFF(void* pvParameters)
+{
+	for (;;)
+	{
+		prvLed_Toggle(0x0);
+		taskYIELD();
+	}
+}
+
+void prvLed_Toggle(int Mode)
+{
+	portTickType xNextWakeTime;
+	static u32 Data = 1;
+
+	xSemaphoreTake(xMutex_Led, portMAX_DELAY);
+
+	// Set the GPIO Output for each 4 LEDs to the value of Mode
+	if (Mode == 0) Data<<=1;
+	if (Data == 16) Data = 1;
+	XGpio_DiscreteWrite(&GpioOutput, LED_CHANNEL, Mode?Data:0);
+
+	if (Mode == 0x1)
+		xil_printf("LED ON\r\n");
+	else
+		xil_printf("LED OFF\r\n");
+
+	// Initialize xNextWakeTime - this only needs to be done once.
+	xNextWakeTime = xTaskGetTickCount();
+
+	// Place this task in the blocked state until it is time to run again.
+	// The block time is specified in ticks, the constant used converts ticks
+	// to ms. While in the Blocked state this task will not consume any CPU
+	// time.
+	vTaskDelayUntil(&xNextWakeTime, BLINK_PERIOD);
+
+	xSemaphoreGive(xMutex_Led);
+}
+
+int prvSetGpioHardware(void)
+{
+	int Status;
+	// Initialize the GPIO driver so that it's ready to use,
+	// specify the device ID that is generated in xparameters.h
+	Status = XGpio_Initialize(&GpioOutput, GPIO_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	// Set the direction for all signals to be outputs
+	XGpio_SetDataDirection(&GpioOutput, LED_CHANNEL, 0x0);
+
+	// Set the GPIO outputs to low
+	XGpio_DiscreteWrite(&GpioOutput, LED_CHANNEL, 0x0);
+	return Status;
+}
---
+++ My_Project/apps/freertos_blink_led_semaphore/freertos_blink_led_semaphore.c
@@ -0,0 +1,147 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "timers.h"
+
+#include "xil_printf.h"
+#include "xil_cache.h"
+#include "xparameters.h"
+#include "xstatus.h"
+#include "xgpio.h"
+
+#define GPIO_DEVICE_ID	XPAR_LEDS_4BITS_DEVICE_ID
+
+#define LED_CHANNEL	1
+
+#define mainLED_TASK_PRIORITY	(tskIDLE_PRIORITY + 1)
+
+#define TIMER_PERIOD 10
+#define TIMER_ID	123
+
+XGpio GpioOutput;
+
+xTimerHandle xTimer;
+
+xTaskHandle xTask;
+
+xSemaphoreHandle xSemaphore_led = NULL;
+
+static void prvLed_Task(void* pvParameters);
+int prvSetGpioHardware(void);
+void prvShutdown(void);
+
+void vTimerCallback(xTimerHandle pxTimer)
+{
+	if (xSemaphoreGive(xSemaphore_led) != pdTRUE)
+	{
+		xil_printf("xSemaphore_led give fail\r\n");
+		prvShutdown();
+	}
+}
+
+int main(void)
+{
+    microblaze_disable_interrupts();
+
+    #if defined( XPAR_MICROBLAZE_USE_ICACHE ) && ( XPAR_MICROBLAZE_USE_ICACHE != 0 )
+    {
+        Xil_ICacheInvalidate();
+        Xil_ICacheEnable();
+    }
+    #endif
+
+    #if defined( XPAR_MICROBLAZE_USE_DCACHE ) && ( XPAR_MICROBLAZE_USE_DCACHE != 0 )
+    {
+        Xil_DCacheInvalidate();
+        Xil_DCacheEnable();
+    }
+    #endif
+
+	// Create Binary Semaphore
+	vSemaphoreCreateBinary(xSemaphore_led);
+	configASSERT(xSemaphore_led);
+
+	// Setup the GPIO Hardware
+	prvSetGpioHardware();
+
+	// Create the task
+	xTaskCreate(prvLed_Task, "LED_TASK", configMINIMAL_STACK_SIZE, NULL,
+				mainLED_TASK_PRIORITY, &xTask);
+
+	// Create timer. Starting the timer before the scheduler
+	// has been started means the timer will start running immediately that
+	// the scheduler starts.
+	xTimer = xTimerCreate("LedTimer", TIMER_PERIOD, 
+			pdTRUE, // auto-reload when expires
+			(void*)TIMER_ID, // unique id
+			vTimerCallback // Callback
+			);
+	
+	if (xTimer == NULL) {
+		// The timer was not created.
+		xil_printf("Failed to create timer\r\n");
+		prvShutdown();
+		return 0;
+	} else {
+		// Start the timer
+		xTimerStart(xTimer, 0);
+	}
+
+	// Starting the scheduler will start the timers running as it is already
+	// been set into the active state.
+	vTaskStartScheduler();
+
+	// Should not reach here.
+	for (;;);
+
+	return 0;
+}
+
+static void prvLed_Task(void* pvParameters)
+{
+	unsigned int uiLedFlag = 0;
+	static u32 Data = 1;
+
+	for (;;)
+	{
+		if (xSemaphoreTake(xSemaphore_led, portMAX_DELAY) == pdTRUE)
+		{
+			uiLedFlag ^= 1;
+			if (uiLedFlag == 0) Data<<=1;
+			if (Data == 16) Data = 1;
+			XGpio_DiscreteWrite(&GpioOutput, LED_CHANNEL, uiLedFlag?Data:0);
+		} else {
+			xil_printf("xSemaphore_led take fail\r\n");
+			// Call shutdown
+			prvShutdown();
+		}
+	}
+}
+
+int prvSetGpioHardware(void)
+{
+	int Status;
+	// Initialize the GPIO driver so that it's ready to use,
+	// specify the device ID that is generated in xparameters.h
+	Status = XGpio_Initialize(&GpioOutput, GPIO_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	// Set the direction for all signals to be outputs
+	XGpio_SetDataDirection(&GpioOutput, LED_CHANNEL, 0x0);
+
+	// Set the GPIO outputs to low
+	XGpio_DiscreteWrite(&GpioOutput, LED_CHANNEL, 0x0);
+
+	return Status;
+}
+
+void prvShutdown(void)
+{
+	// Check if timer is created
+	if (xTimer)
+		xTimerDelete(xTimer, 0);
+	vSemaphoreDelete(xSemaphore_led);
+	vTaskDelete(xTask);
+}
